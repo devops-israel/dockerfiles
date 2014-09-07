2e066c52b7cc45894acc423478cc9773cc045c1e
Evgeny Zislis <evgeny.zislis@gmail.com>
2014-09-07 15:20:55 +0300

    re-create latest version as a tag as well

--- docker-puppet/versions.sh
+++ docker-puppet/versions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for v in `tail -n +2 versions.txt`; do
+for v in `cat versions.txt`; do
     mkdir -p versions/$v
     sed -e "s/ENV PUPPET_VERSION .*$/ENV PUPPET_VERSION $v/" Dockerfile > versions/$v/Dockerfile
 done
